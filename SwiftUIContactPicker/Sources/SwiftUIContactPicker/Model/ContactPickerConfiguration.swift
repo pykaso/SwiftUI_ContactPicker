e70a451c804e957bfcf04f2ebc46b92ea9022d97
Lukas Gergel <lukas.gergel@zonky.cz>
2020-11-08 17:36:17 +0100

    public init for l10n struct 🤦‍♂️

--- SwiftUIContactPicker/Sources/SwiftUIContactPicker/Model/ContactPickerConfiguration.swift
+++ SwiftUIContactPicker/Sources/SwiftUIContactPicker/Model/ContactPickerConfiguration.swift
@@ -1,14 +1,21 @@
 
 public struct ContactPickerConfiguration {
     public static let `default`: ContactPickerConfiguration = ContactPickerConfiguration(l10n: L10n(title: "Contacts",
-                                                                                                cancelButton: "Cancel",
-                                                                                                groupsButton: "Groups",
-                                                                                                searchPlaceholder: "Search"))
+                                                                                                    cancelButton: "Cancel",
+                                                                                                    groupsButton: "Groups",
+                                                                                                    searchPlaceholder: "Search"))
     public init(l10n: ContactPickerConfiguration.L10n) {
         self.l10n = l10n
     }
-    
+
     public struct L10n {
+        public init(title: String, cancelButton: String, groupsButton: String, searchPlaceholder: String) {
+            self.title = title
+            self.cancelButton = cancelButton
+            self.groupsButton = groupsButton
+            self.searchPlaceholder = searchPlaceholder
+        }
+
         let title: String
         let cancelButton: String
         let groupsButton: String
